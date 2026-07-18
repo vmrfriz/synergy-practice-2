2a6d45b103920c6235f9ab3973bcc50660783122
Valeriy Grechukha <v.mrfriz@gmail.com>
2026-07-18 16:13:15 +0300

    fix: Comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
 
 /**
  * Комментарий к записи блога
  *
- * @property string $ulid
+ * @property string $id
+ * @property int $post_id
  * @property int $author_id
  * @property string $content
  * @property Carbon $created_at
@@ -24,6 +27,15 @@ use Illuminate\Support\Carbon;
 #[Fillable(['content'])]
 class Comment extends Model
 {
+    use HasFactory;
+
+    /** @return BelongsTo<Post, Comment> */
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    /** @return HasMany<User, Comment> */
     public function author(): HasMany
     {
         return $this->hasMany(User::class, 'author_id');
