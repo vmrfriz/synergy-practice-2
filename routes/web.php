b60d016e9b0ec6c1e7d6c677cb7b980a69dcb789
Valeriy Grechukha <v.mrfriz@gmail.com>
2026-07-19 11:32:29 +0300

    Обновлены роуты

--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,27 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-Route::get('/', function () {
-    return Inertia::render('Home', [
-        'posts' => [],
-    ]);
-});
 
 Route::middleware('guest')->group(function () {
-    Route::get('/login', function () {
-        return Inertia::render('Auth/Login');
-    });
-    Route::get('/register', function () {
-        return Inertia::render('Auth/Register');
-    });
+    Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/register', [AuthController::class, 'registerForm'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', function () {
-        return Inertia::render('Profile', [
-            'posts' => [],
-            'subscriptions' => [],
-            'feedPosts' => [],
-        ]);
-    });
-    Route::get('/posts/create', function () {
-        //
-    });
-    Route::post('/posts', function () {
-        //
-    });
-    Route::get('/posts/{post:ulid}/edit', function () {
-        //
-    });
-    Route::delete('/posts/{post:ulid}', function () {
-        //
-    });
+Route::middleware('auth')->group(static function () {
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+
+    // Route::post('/author/{user}/{post:slug}/comment', [CommentController::class, 'store'])->name('author.posts.comment');
 });
 
-Route::get('/posts/{post:ulid}', function () {
-    //
-});
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('/tags/{tag:name}', [PostController::class, 'tag'])->name('tag');
+Route::get('/author/{user}', [PostController::class, 'author'])->name('author.posts');
+Route::post('/author/{user}/subscribe', [ProfileController::class, 'subscribe'])->name('profile.subscribe');
+Route::post('/author/{user}/unsubscribe', [ProfileController::class, 'unsubscribe'])->name('profile.unsubscribe');
+Route::get('/author/{user}/{post:slug}', [PostController::class, 'show'])->name('author.posts.show');
