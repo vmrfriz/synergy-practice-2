812935f87affc5d8f38064d7b556b3c66765b7f0
Valeriy Grechukha <v.mrfriz@gmail.com>
2026-07-26 13:33:11 +0300

    Доработан компонент формы комментария

--- resources/js/Components/Comments/CommentForm.jsx
+++ resources/js/Components/Comments/CommentForm.jsx
@@ -1,27 +1,37 @@
-import { useForm } from "@inertiajs/react";
+import { useForm, usePage } from "@inertiajs/react";
 import { storeCommentUrl } from "../../route";
 
 export default function CommentForm(props) {
-    const { data, post, processing, errors } = useForm({
+    const { data, setData, reset, post, processing, errors } = useForm({
         content: '',
     });
 
-    const send = () => post(storeCommentUrl(props.post));
+    const { auth } = usePage().props;
+
+    if (! auth.user) {
+        return null;
+    }
+
+    const send = () => post(storeCommentUrl(props.post), { onSuccess: () => reset() });
 
     return (
         <div>
             <textarea
-                className="form-control mb-2"
+                className={`form-control mb-2 ${errors.content ? 'is-invalid' : ''}`}
                 placeholder="Текст комментария"
-                onChange={(e) => data.content = e.target.value}
-            >
-                {data.content}
-            </textarea>
+                value={data.content}
+                onChange={(e) => setData('content', e.target.value)}
+            />
+            {errors.content && <div className="invalid-feedback mb-2">{errors.content}</div>}
+
             <button
                 type="button"
                 onClick={send}
+                disabled={processing}
                 className={`btn btn-primary ${processing ? 'progress-bar progress-bar-striped progress-bar-animated' : ''}`.trim()}
-            >Отправить</button>
+            >
+                Отправить
+            </button>
         </div>
     );
 };
